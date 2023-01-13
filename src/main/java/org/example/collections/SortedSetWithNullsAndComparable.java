069fca74ddc70d7a95b13bb0e6af0dc1f15dc240
Anton_Lukyanau1 <anton_lukyanau1@epam.com>
2023-01-13 16:08:13 +0100

    TreeSet and null elements. Comparator vs Comparable

--- src/main/java/org/example/collections/SortedSetWithNullsAndComparable.java
+++ src/main/java/org/example/collections/SortedSetWithNullsAndComparable.java
@@ -3,9 +3,28 @@ package org.example.collections;
 import java.io.IOException;
 import java.util.*;
 
+/**
+ Задача: Написать компаратор, который позволил бы TreeSet хранить null. Попробовать реализовать Comparable с той же целью.
+ */
 public class SortedSetWithNullsAndComparable {
 
     public static void main(String[] args) throws ClassNotFoundException, IOException {
+        SortedSet<A> sortedSetWithNulls = new TreeSet<>((elem1, elem2) -> {
+            if (elem1 == null && elem2 == null) {
+                return 0;
+            }
+            if (elem1 == null){
+                return -1;
+            }
+            if (elem2 == null) {
+                return 1;
+            }
+            return elem1.hashCode() - elem2.hashCode();
+        });
+        sortedSetWithNulls.add(null);
+        sortedSetWithNulls.add(null);
+        System.out.println(sortedSetWithNulls);
+
         SortedSet<A> sortedSet = new TreeSet<>();
         sortedSet.add(null);
         sortedSet.add(null);
@@ -18,7 +37,7 @@ public class SortedSetWithNullsAndComparable {
             if (o == null) {
                 return 1;
             }
-            return this.hashCode() - o.hashCode();
+            return Objects.hash(this) - Objects.hash(o);
         }
     }
 
